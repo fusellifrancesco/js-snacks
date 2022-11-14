8d93b4b889383bc3b3eff3ef23f777218e593dbd
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-14 16:13:46 +0100

    Inserito ciclo FOR e condizione IF

--- JSNACK4/script.js
+++ JSNACK4/script.js
@@ -1,2 +1,19 @@
-// Dichiaro un array
-let nomiInvitati = ["Marco", "Alessio", "Giorgia", "Andrea", "Luigi"];
+// Dichiaro un array e chiedo all'utente il suo nome
+let arrayNomiInvitati = ["Marco", "Alessio", "Giorgia", "Andrea", "Luigi"];
+let nomeUtente = prompt("Inserisci il tuo nome");
+let nomeUtentePresenteInLista = false
+
+// Controllo se il nome dell'utente è presente nella lista
+for (let i = 0; i < arrayNomiInvitati.length; i++) {
+    
+    if(nomeUtente == arrayNomiInvitati[i]){
+        console.log("BENVENUTO ALLA FESTA " + nomeUtente + " !!!");
+        nomeUtentePresenteInLista = true;
+        break;
+    }
+}
+
+if (!nomeUtentePresenteInLista){
+    console.log("Mi Dispiace ma il tuo nome non è presente sulla lista");
+}
+
