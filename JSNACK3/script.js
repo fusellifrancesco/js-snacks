673994122700aa8e319d1f2f5cd94b51544377ad
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-14 15:50:47 +0100

    dichiaro variabili, converto le stringhe in numeri

---
+++ JSNACK3/script.js
@@ -0,0 +1,16 @@
+// Chiedo l'input all'utente di 10 numeri
+let numero1 = prompt("Inserisci un numero casuale");
+let numero2 = prompt("Inserisci un numero casuale");
+let numero3 = prompt("Inserisci un numero casuale");
+let numero4 = prompt("Inserisci un numero casuale");
+let numero5 = prompt("Inserisci un numero casuale");
+let numero6 = prompt("Inserisci un numero casuale");
+let numero7 = prompt("Inserisci un numero casuale");
+let numero8 = prompt("Inserisci un numero casuale");
+let numero9 = prompt("Inserisci un numero casuale");
+let numero10 = prompt("Inserisci un numero casuale");
+
+// Converto le stringhe in numeri, le sommo e stampo il risultato
+let somma = Number(numero1)+Number(numero2)+Number(numero3)+Number(numero4)+Number(numero5)+
+            Number(numero6)+Number(numero7)+Number(numero8)+Number(numero9)+Number(numero10);
+
