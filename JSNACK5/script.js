6ebaf560dac89d997ec4ee566769cd716c0ead8f
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-14 17:18:00 +0100

    aggiunto switch

--- JSNACK5/script.js
+++ JSNACK5/script.js
@@ -9,8 +9,37 @@ let numero4 = prompt("Inserisci un numero");
 let numero5 = prompt("Inserisci un numero");
 let numero6 = prompt("Inserisci un numero");
 
-if ((numero1 % 2) = 0){
-    arrayNumeri.push(numero1);
-    console.log(arrayNumeri);
+let RestoNumero1 = numero1 % 2;
+let RestoNumero2 = numero2 % 2;
+let RestoNumero3 = numero3 % 2;
+let RestoNumero4 = numero4 % 2;
+let RestoNumero5 = numero5 % 2;
+let RestoNumero6 = numero6 % 2;
+
+switch (arrayNumeri){
+    case (RestoNumero1 = 0):
+        arrayNumeri.push(numero1);
+    
+    case (RestoNumero2 = 0):
+        arrayNumeri.push(numero2);
+
+    case (RestoNumero3 = 0):
+        arrayNumeri.push(numero3);
+    
+    case (RestoNumero4 = 0):
+        arrayNumeri.push(numero4);
+
+    case (RestoNumero5 = 0):
+        arrayNumeri.push(numero5);
+
+    case (RestoNumero6 = 0):
+        arrayNumeri.push(numero6);
+
 }
 
+console.log(
+    `
+    ------La nuova array è------
+    ${arrayNumeri}
+    `
+)
