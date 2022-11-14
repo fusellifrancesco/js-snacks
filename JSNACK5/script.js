02bb98c78349bb623da27dc02e3a2cc51dcaa44c
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-14 17:58:06 +0100

    aggiunta ciclo FOR Jsnack5

--- JSNACK5/script.js
+++ JSNACK5/script.js
@@ -1,6 +1,20 @@
 // Dichiaro un array vuoto
 let arrayNumeri = [];
 
+for (i = 0; i < 6; i++){
+    let numero = prompt("Inserisci un numero");
+    let restoNumero = numero % 2;
+    
+    if (restoNumero != 0){
+        arrayNumeri.push(numero);
+    }
+    
+}
+
+console.log(arrayNumeri);
+
+
+/*
 // Chiedo l'input all'utente
 let numero1 = prompt("Inserisci un numero");
 let numero2 = prompt("Inserisci un numero");
@@ -9,12 +23,30 @@ let numero4 = prompt("Inserisci un numero");
 let numero5 = prompt("Inserisci un numero");
 let numero6 = prompt("Inserisci un numero");
 
-let RestoNumero1 = numero1 % 2;
-let RestoNumero2 = numero2 % 2;
-let RestoNumero3 = numero3 % 2;
-let RestoNumero4 = numero4 % 2;
-let RestoNumero5 = numero5 % 2;
-let RestoNumero6 = numero6 % 2;
+let restoNumero1 = numero1 % 2;
+let restoNumero2 = numero2 % 2;
+let restoNumero3 = numero3 % 2;
+let restoNumero4 = numero4 % 2;
+let restoNumero5 = numero5 % 2;
+let restoNumero6 = numero6 % 2;
+
+if (restoNumero1 == 0){
+    arrayNumeri.push(numero1);
+} else if (restoNumero2 == 0){
+    arrayNumeri.push(numero2);
+} else if (restoNumero3 == 0){
+    arrayNumeri.push(numero3);
+} else if (restoNumero4 == 0){
+    arrayNumeri.push(numero4);
+} else if (restoNumero)
+
+
+console.log(
+    `
+    ------La nuova array è------
+    ${arrayNumeri}
+    `
+)
 
 switch (arrayNumeri){
     case (RestoNumero1 = 0):
@@ -36,10 +68,4 @@ switch (arrayNumeri){
         arrayNumeri.push(numero6);
 
 }
-
-console.log(
-    `
-    ------La nuova array è------
-    ${arrayNumeri}
-    `
-)
+*/
