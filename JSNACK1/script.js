b4ac3b0883d97a1aaf3d9e1b7decd11358276bd7
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-14 15:08:39 +0100

    Dichiarato variabili e condizione IF Jsnack1

---
+++ JSNACK1/script.js
@@ -0,0 +1,8 @@
+let numero1 = prompt("Inserisci un numero");
+let numero2 = prompt("Inserisci un altro numero");
+
+if (numero1 > numero2){
+    console.log(numero1);
+} else {
+    console.log(numero2);
+}
