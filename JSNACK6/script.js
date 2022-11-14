798153c4872f3442ed7cf465fa3fe908e9a47f57
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-14 17:32:01 +0100

    stampo i numeri e il risultato del gioco

--- JSNACK6/script.js
+++ JSNACK6/script.js
@@ -5,3 +5,12 @@ let numeroUtente = prompt("Inserisci un numero da 0 a 10");
 let numeroRandomVirgola = Math.random() * 11;
 let numeroRandomIntero = Math.floor(numeroRandomVirgola);
 
+// Stampo i numeri del computer e dell'utente e il risultato del gioco
+console.log("Il numero scelto dal Computer è: " + numeroRandomIntero);
+console.log("Il numero scelto da te è: " + numeroUtente);
+
+if (numeroUtente == numeroRandomIntero){
+    console.log("HAI VINTO");
+} else {
+    console.log("HAI PERSO");
+}
