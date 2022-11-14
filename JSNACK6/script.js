4b898a364b18a4cf1e6dd5f188c494c699944be0
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-14 17:31:34 +0100

    chiedo Input utente e genero numero random Jsnack6

---
+++ JSNACK6/script.js
@@ -0,0 +1,7 @@
+// Chiedo l'input all'utente di un numero
+let numeroUtente = prompt("Inserisci un numero da 0 a 10");
+
+// Chiedo al computer di generare un numero random tra 0 e 10
+let numeroRandomVirgola = Math.random() * 11;
+let numeroRandomIntero = Math.floor(numeroRandomVirgola);
+
